ee7e9e60d6f6d5ce929ee4414d1876ce37b68d05
Jvictor_077 <joaovictortwrp@gmail.com>
2026-03-03 19:34:33 -0300

    Atualização
    Configuração das credencias

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyC6ksyPJSaeQ9r9xDebCO8WWMF1grv-dqo",
   authDomain: "pi-3p-tads049.firebaseapp.com",
   projectId: "pi-3p-tads049",
-  storageBucket: "pi-3p-tads049.appspot.com",
-  messagingSenderId: "",
-  appId: "",
+  storageBucket: "pi-3p-tads049.firebasestorage.app",
+  messagingSenderId: "280565366050",
+  appId: "1:280565366050:web:77677298e50b975fb5797b"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
